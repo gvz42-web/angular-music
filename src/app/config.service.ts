9041c4a6bf49254eea39db5a017c44ac5f85d94a
gvz42-web <gvz42.web@gmail.com>
2024-01-18 19:17:25 +0200

    Change default key to C minor and one octave down

--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -6,7 +6,7 @@ import {BehaviorSubject, Observable} from "rxjs";
 })
 export class ConfigService {
   private _speed = new BehaviorSubject(10)
-  private _notes  =  new BehaviorSubject(['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5'])
+  private _notes  =  new BehaviorSubject(['C3', 'D3', 'D#3', 'F3', 'G3', 'G#3', 'A#3', 'C4'])
   private _minHeight= new BehaviorSubject(20)
   private _dh = new BehaviorSubject(20)
 
